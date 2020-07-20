4edd5431596e754dcfd089bd8ac213c2238af987
Amkam <amaduskamara@gmail.com>
2020-07-20 21:52:35 +0000

    Added pages and Navigation Links to the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+
+  # get '/' => 'pages#home' 
+  get 'about' => 'pages#about'
+  get 'contact' => 'pages#contact'
+  get 'posts' => 'pages#posts'
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html - DSL Means - Domain Specific Language
 end
